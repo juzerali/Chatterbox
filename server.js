1ffe10375c59f4328a8898cc65f3d3534aa1610d
unknown <Juzer@Juzer-PC.(none)>
2012-03-31 22:43:23 +0530

    msgsddfsdffddsdsdsfd

--- server.js
+++ server.js
@@ -38,3 +38,4 @@ console.log("Express server listening on port %d in %s mode", app.address().port
  everyone.now.distributeMessage = function(message){
   everyone.now.receiveMessage(this.now.name, message);
 };	
+	
